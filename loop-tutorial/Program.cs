9bf2004345e29434b752323a68f81981232e32f6
drincon2 <diegorima98@hotmail.com>
2021-02-10 00:10:50 -0700

    Brief tutorial of branches and loops

---
+++ loop-tutorial/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace BranchesAndLoops
+{
+    class Program
+    {
+        static void ExploreIf()
+        {
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+            {
+                Console.WriteLine("The answer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+            }
+
+            int c = 4;
+            if ((a + b + c > 10) && (a > b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not greater than the second");
+            }
+
+            if ((a + b + c > 10) || (a > b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is greater than the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not greater than the second");
+            }
+        }
+
+        static void WorkingWithLoops()
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
+            
+            int c = 0;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {c}");
+                c++;
+            } while (c < 10);
+
+            for (int index = 0; index < 10; index++)
+            {
+                Console.WriteLine($"Hello World! The index is {index}");
+            }
+
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column})");
+                }
+            }
+        }
+
+        static void findSumDivisible()
+        {
+            for (int a = 0; a < 21; a++) 
+            {
+                for (int b = 0; b < 21; b++)
+                {
+                    if((a+b)%3 == 0) 
+                    {
+                        Console.WriteLine(a + " " + b);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            ExploreIf();
+
+            WorkingWithLoops();
+
+            findSumDivisible();
+
+        }
+    }
+}
